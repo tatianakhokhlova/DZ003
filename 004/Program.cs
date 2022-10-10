a153f3d2f6fda366f05e28645f646a17d9ece96c
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-10 14:50:53 +0300

    показать кубы чисел, заканчивающихся на четную цифру

---
+++ 004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+while (a<=N)
+{
+    int b = a * a * a;                          
+    int c = b % 10;                             
+    if(c % 2 == 0)                              
+    {
+    Console.WriteLine($"Число {a} в кубе  {b}.");
+    }
+    a++;
+
+}
