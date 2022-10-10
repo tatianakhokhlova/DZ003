cdf515809ee77a712450ba57b480ce095c7ab690
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-10 14:43:48 +0300

    Написать программу вычисления произведения чисел от 1 до N

---
+++ 003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+int result = 1;
+int i = 1;
+
+while(i <= N){
+    result = result * i;
+    i = i + 1;
+}
+
+Console.WriteLine(result);
